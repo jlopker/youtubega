57bffbff4865b174baa1a7f0dedcd27a720d6497
Juliana <jlopker@gmail.com>
2015-06-30 12:57:12 -0700

    added youtube functionality

--- public/dist/application.js
+++ public/dist/application.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'youtubega';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'youtube-embed'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
@@ -86,12 +86,43 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
-	}
-]);
+angular.module('core').controller('HomeController', ['$scope', '$http',
+    function($scope, $http) {
+
+        $scope.search = function(){
+            
+            if ( typeof $scope.keyword === 'undefined' || $scope.keyword === ''){
+              
+              $scope.error = 'No videos';
+              $scope.videos = null;
+              
+          } else {
+              
+              $scope.error = null;
+              
+              $http.get('https://www.googleapis.com/youtube/v3/search',{
+                  params: {
+                    key: 'AIzaSyACTxZmBK4_-UBqoxKzojXgmPZkTNWP5U8',
+                    type: 'video',
+                    maxResults : '12',
+                    part: 'id, snippet',
+                    fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle',
+                    q: $scope.keyword
+                }
+            })
+              .success( function (data) {
+                  $scope.keyword = '';
+                  $scope.videos = data.items;
+              })
+              .error( function (){
+                  $scope.keyword = '';
+              });
+              
+          }
+          
+      };
+  }
+  ]);
 'use strict';
 
 //Menu service used for managing  menus
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,40 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
-	}
-]);
+angular.module('core').controller('HomeController', ['$scope', '$http',
+    function($scope, $http) {
+
+        $scope.search = function(){
+            
+            if ( typeof $scope.keyword === 'undefined' || $scope.keyword === ''){
+              
+              $scope.error = 'No videos';
+              $scope.videos = null;
+              
+          } else {
+              
+              $scope.error = null;
+              
+              $http.get('https://www.googleapis.com/youtube/v3/search',{
+                  params: {
+                    key: 'AIzaSyACTxZmBK4_-UBqoxKzojXgmPZkTNWP5U8',
+                    type: 'video',
+                    maxResults : '12',
+                    part: 'id, snippet',
+                    fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle',
+                    q: $scope.keyword
+                }
+            })
+              .success( function (data) {
+                  $scope.keyword = '';
+                  $scope.videos = data.items;
+              })
+              .error( function (){
+                  $scope.keyword = '';
+              });
+              
+          }
+          
+      };
+  }
+  ]);
